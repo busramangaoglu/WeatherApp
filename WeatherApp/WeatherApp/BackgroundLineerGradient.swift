6f5b0ec303b3389ff0e91a9901f78dd7059c2e90
Büşra Mangaoğlu <mangabusraa@gmail.com>
2026-07-10 13:13:56 +0300

    Initial project setup

---
+++ WeatherApp/WeatherApp/BackgroundLineerGradient.swift
@@ -0,0 +1,26 @@
+//
+//  Background.swift
+//  WeatherApp
+//
+//  Created by Büşra Mangaoğlu on 10.07.2026.
+//
+
+import SwiftUI
+
+struct BackgroundLineerGradient: View {
+    var body: some View {
+        LinearGradient(
+            colors: [
+                Color.purple.opacity(0.2),
+                Color.purple
+            ],
+            startPoint: .top,
+            endPoint: .bottom
+        )
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    BackgroundLineerGradient()
+}
