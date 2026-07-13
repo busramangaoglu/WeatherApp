770edd31c8c9b9e824675c56147e86d629f29f65
Büşra Mangaoğlu <mangabusraa@gmail.com>
2026-07-13 11:28:23 +0300

    fix

---
+++ WeatherApp/Views/HourlyForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  HourlyForecastView.swift
+//  WeatherApp
+//
+//  Created by Büşra Mangaoğlu on 13.07.2026.
+//
+
+import SwiftUI
+
+struct HourlyForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HourlyForecastView()
+}
